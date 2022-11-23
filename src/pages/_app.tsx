1f25cbed3d44a3f585b0ebd2995d580597174641
cpt4567 <cpt4567@gmail.com>
2022-11-23 14:28:35 +0900

    as

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,22 @@ import type { AppProps } from 'next/app';
 import HeaderLayout from '../components/layout/Header/HeaderLayout';
 import Seo from '../components/layout/Header/Title';
 import Script from 'next/script';
+import { Map ,MapMarker } from  "react-kakao-maps-sdk"
 
 
 export default function App({ Component, pageProps }: AppProps) {
   // 이안에 header랑 footer 넣을꺼임
 
+  const NEXT_PUBLIC_KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_KEY;
+
   return (
     <>
       {/* kakao api */}
       <Script
-        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=0cb1b7e90d83dfaacea34b3204371e72&libraries=services,clusterer&autoload=false"
+        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_KEY}&libraries=services,clusterer&autoload=false`}
         strategy="beforeInteractive"
       />
-
+    
       <Seo disabled={true} title="main" />
 
       <Component {...pageProps} />
